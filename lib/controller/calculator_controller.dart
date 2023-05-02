e0f61a60879ebb73af0ba8a1703f95ada55c3ca5
Artur <sribnyi@gmail.com>
2023-05-02 03:04:03 +0300

    fixed the decimal calculations problem - added the Decimal and Rational

--- lib/controller/calculator_controller.dart
+++ lib/controller/calculator_controller.dart
@@ -1,9 +1,33 @@
+import 'dart:convert';
+
+import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import '../model/calculator_model.dart';
 
 class CalculatorController {
+  final Logger logger = Logger();
   final CalculatorModel _model;
 
   CalculatorController(this._model);
+
+  void addToHistory(String input, String output) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final history = prefs.getStringList('history') ?? [];
+
+      final historyItem = {
+        'input': input,
+        'output': output,
+        'timestamp': DateTime.now().toIso8601String(),
+      };
+
+      history.add(jsonEncode(historyItem));
+      prefs.setStringList('history', history);
+    } catch (e) {
+      logger.e(e);
+    }
+  }
 
   void buttonPressed(String buttonText) {
     switch (buttonText) {
@@ -17,18 +41,19 @@ class CalculatorController {
         _model.appendToInput(buttonText);
         break;
       case ".":
-        _model.addDecimal();
+        _model.appendToInput(buttonText);
         break;
       case "=":
         _model.calculate();
         if (_model.input.isNotEmpty && _model.output.isNotEmpty) {
-          _model.addToHistory(_model.input, _model.output);
+          addToHistory(_model.input, _model.output);
         }
-        _model.clearInput(); // Clear the input after the calculation
+
+        /// Clear the input after the calculation
+        _model.clearInput();
         break;
       default:
         _model.appendToInput(buttonText);
     }
-    // return _model.output;
   }
 }
--- lib/model/calculator_model.dart
+++ lib/model/calculator_model.dart
@@ -1,109 +1,108 @@
-import 'package:flutter/foundation.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
+import 'package:decimal/decimal.dart';
 import 'package:logger/logger.dart';
+import 'package:rational/rational.dart';
 
 class CalculatorModel {
   final Logger logger = Logger();
 
   String _input = '';
   String _output = '0';
-  double _num1 = 0.0;
-  double _num2 = 0.0;
   String _operator = '';
 
   String get input => _input;
 
   String get output => _output;
 
-  double get num1 => _num1;
-
-  double get num2 => _num2;
-
   String get operator => _operator;
 
   void clear() {
     _input = '';
     _output = '0';
-    _num1 = 0.0;
-    _num2 = 0.0;
     _operator = '';
   }
 
-  void setOperator(String operator) {
-    _num1 = double.parse(_output);
-    _operator = operator;
-    _input = '$_num1 $operator ';
-    _output = "0";
-  }
-
-  void addDecimal() {
-    if (!_output.contains(".")) {
-      _output += ".";
-    }
-  }
+  // /// new calculate method to support more than two numbers/operators
+  // void calculate() {
+  //   logger.i(_input);
+  //
+  //   /// Regexp filters out operators: '-' subtraction (escaped the '-' symbol
+  //   /// with a '\'), '+' addition, '*' multiplication, '/' division
+  //   List<String> digits = _input.split(RegExp(r'[+\-*/]'));
+  //
+  //   /// RegExp filter out numbers. Range of [0 to 9] followed by a '.' to
+  //   /// include decimal numbers. The '+' to include all preceding matches
+  //   List<String> operators =
+  //       _input.split(RegExp(r'[0-9.]+')).where((e) => e.isNotEmpty).toList();
+  //
+  //   /// removing the first element of the operators list if it's empty
+  //   double result = double.parse(digits[0]);
+  //
+  //   for (int i = 0; i < operators.length; i++) {
+  //     double nextNumber = double.parse(digits[i + 1]);
+  //     String operator = operators[i];
+  //
+  //     switch (operator) {
+  //       case "+":
+  //         result += nextNumber;
+  //         break;
+  //       case "-":
+  //         result -= nextNumber;
+  //         break;
+  //       case "*":
+  //         result *= nextNumber;
+  //         break;
+  //       case "/":
+  //         result /= nextNumber;
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //   }
 
   /// new calculate method to support more than two numbers/operators
   void calculate() {
+    logger.i(_input);
+
     /// Regexp filters out operators: '-' subtraction (escaped the '-' symbol
     /// with a '\'), '+' addition, '*' multiplication, '/' division
     List<String> digits = _input.split(RegExp(r'[+\-*/]'));
 
     /// RegExp filter out numbers. Range of [0 to 9] followed by a '.' to
     /// include decimal numbers. The '+' to include all preceding matches
-    List<String> operators = _input.split(RegExp(r'[0-9.]+'));
+    List<String> operators =
+        _input.split(RegExp(r'[0-9.]+')).where((e) => e.isNotEmpty).toList();
 
     /// removing the first element of the operators list if it's empty
-    if (operators.isNotEmpty && operators.first.isEmpty) {
-      operators.removeAt(0);
-    }
-
-    double result = double.parse(digits[0]);
-
+    Decimal result = Decimal.parse(digits[0]);
 
     for (int i = 0; i < operators.length; i++) {
-      switch (operators[i]) {
+      Decimal nextNumber = Decimal.parse(digits[i + 1]);
+      String operator = operators[i];
+
+      switch (operator) {
         case "+":
-          result += double.parse(digits[i + 1]);
+          result += nextNumber;
           break;
         case "-":
-          result -= double.parse(digits[i + 1]);
+          result -= nextNumber;
           break;
         case "*":
-          result *= double.parse(digits[i + 1]);
+          result *= nextNumber;
           break;
         case "/":
-          result /= double.parse(digits[i + 1]);
+          logger.i(result / nextNumber);
+          Rational r = result / nextNumber;
+          result = r.toDecimal(scaleOnInfinitePrecision: 8);
           break;
         default:
           break;
       }
     }
 
+    /// showing off out my ternary conditional expression skill
     _output = result.toString();
-
-    logger.d(digits.toString());
-    logger.d(operators.toString());
-  }
-
-  void addToHistory(String input, String output) async {
-    try {
-      final prefs = await SharedPreferences.getInstance();
-      final history = prefs.getStringList('history') ?? [];
-
-      final historyItem = {
-        'input': input,
-        'output': output,
-        'timestamp': DateTime.now().toIso8601String(),
-      };
-
-      history.add(jsonEncode(historyItem));
-      prefs.setStringList('history', history);
-    } catch (e) {
-      if (kDebugMode) {
-        print("Error saving history: $e");
-      }
-    }
+    logger.i(digits.toString());
+    logger.i(operators.toString());
   }
 
   void appendToInput(String value) {
